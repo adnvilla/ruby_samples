80b743c25fdb5b5552504276d302898795560d2b
adnvilla <adnvilla@gmail.com>
2024-06-13 18:00:28 -0500

    Constants

---
+++ variables/variables.rb
@@ -0,0 +1,11 @@
+MYCONSTANT = "This is a constant variable"
+puts MYCONSTANT
+
+=begin
+MYCONSTANT = "This is a new value for the constant variable"
+
+./variables/variables.rb:5: warning: already initialized constant MYCONSTANT
+./variables/variables.rb:1: warning: previous definition of MYCONSTANT was here
+=end
+
+puts MYCONSTANT
