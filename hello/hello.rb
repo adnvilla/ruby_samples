26478d01e98228f7387123235627e1ab40b64fd1
adnvilla <adnvilla@gmail.com>
2024-06-13 17:54:14 -0500

    Using comments

--- hello/hello.rb
+++ hello/hello.rb
@@ -1,1 +1,18 @@
-print "Hello Ruby!\n"
+# This is a comment line - it explains that the next line of code displays a welcome message
+print "Hello Ruby!\n" # prints the welcome message
+
+# This is a comment line
+# it explains that the next line of code displays 
+# a welcome message
+
+
+=begin
+This is a comment line
+it explains that the next line of code displays 
+a welcome message
+=end
+
+=begin
+print "Welcome to Ruby Essentials!"
+print "Everything you need to know about Ruby"
+=end
